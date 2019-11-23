2b2a364c3b6c213f301b67a5e9a22a9c45956d61
Panda <zpandakst@gearmc.com.br>
2019-11-23 17:36:48 -0300

    Cuzin

--- comandos/anunciar.js
+++ comandos/anunciar.js
@@ -25,5 +25,5 @@ module.exports.run = async (bot, message, args) =>{
 
 module.exports.config = {
     name: 'anuncio',
-    aliases: ['anunciar', 'sendbell', '']
+    aliases: ['anunciar', 'sendbell']
 }
